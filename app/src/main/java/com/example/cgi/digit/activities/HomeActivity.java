2167930915722bdb97fb877f61e256e81645c38b
saikrupa <puram.saikrupa@gmail.com>
2018-01-20 22:03:44 +0530

    Create Door Numbers added

--- app/src/main/java/com/example/cgi/digit/activities/HomeActivity.java
+++ app/src/main/java/com/example/cgi/digit/activities/HomeActivity.java
@@ -3,14 +3,21 @@ package com.example.cgi.digit.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.cgi.digit.R;
+import com.example.cgi.digit.create.door.number.LocationPickActivity;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
  * Created by balapuram on 1/16/2018.
  */
 
 public class HomeActivity extends BaseActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,12 @@ public class HomeActivity extends BaseActivity{
         Intent intent=new Intent(this,MapViewActivity.class);
         startActivity(intent);
     }
+
+    public void createDoorNumber(View view){
+        Intent intent=new Intent(getApplicationContext(), LocationPickActivity.class);
+        startActivity(intent);
+
+    }
+
 
 }
